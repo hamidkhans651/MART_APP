72ca1897bc42eb14f3ddc454c1932986c1b42cad
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-08-16 11:24:00 -0700

    Update main.py

--- order/app/main.py
+++ order/app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return{"hellow":"this is email api"}
+
+@app.get("/crhvrk")
+async def read_root():
+    return{"hellow":"this is email api"}
